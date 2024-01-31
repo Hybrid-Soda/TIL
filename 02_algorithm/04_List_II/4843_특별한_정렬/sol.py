510eb56f295fa83b6e1c7b038672d81a11fcf5be
lim kyoung tae <limkt0925@gmail.com>
2024-02-01 00:07:53 +0900

    18th study COMPLETE

--- 02_algorithm/04_List_II/4843_특별한_정렬/sol.py
+++ 02_algorithm/04_List_II/4843_특별한_정렬/sol.py
@@ -1,11 +1,18 @@
 # 4843 특별한 정렬
 # 주어진 숫자에 대해 특별한 정렬을 한 결과를 10개까지 출력
+# 큰 수와 작은 수를 번갈아 정렬
 
 import sys
 sys.stdin = open('input.txt')
 
 for tc in range(int(input())):
     N = int(input())
+    arr = sorted(list(map(int, input().split())))
+    sort_arr = []
 
+    # 정렬된 배열의 뒤-앞의 숫자를 새로운 배열에 추가
+    for i in range(5):
+        sort_arr.append(arr.pop(-1))
+        sort_arr.append(arr.pop(0))
 
-    print(f'#{tc+1}', arr)
+    print(f'#{tc+1}', *sort_arr)
---
+++ 02_algorithm/05_String/4864_문자열_비교/4864.py
@@ -0,0 +1,17 @@
+# 4864 문자열 비교
+# 문자열 str2 안에 str1과 일치하는 부분이 있는지 찾는 프로그램
+
+import sys
+sys.stdin = open("input.txt")
+
+for tc in range(int(input())):
+    str1 = input()
+    str2 = input()
+
+    # str2에 str1이 있는지 체크
+    if str1 in str2:
+        ans = 1
+    else:
+        ans = 0
+
+    print(f'#{tc+1} {ans}')
