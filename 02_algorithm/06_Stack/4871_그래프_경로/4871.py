a6bfbc806111a9320296b95b7d7e261c8db34b90
임경태 <limkt0925@gmail.com>
2024-02-02 23:48:02 +0900

    algorithm study COMPLETE

--- 02_algorithm/06_Stack/4871_그래프_경로/4871.py
+++ 02_algorithm/06_Stack/4871_그래프_경로/4871.py
@@ -5,15 +5,13 @@ sys.stdin = open("input.txt")
 
 for tc in range(int(input())):
     node_num, route_num = map(int, input().split())
-    line = sorted([list(map(int, input().split())) for _ in range(route_num)])
+    routes = sorted([list(map(int, input().split())) for _ in range(route_num)])
     start, end = map(int, input().split())
-    stack = [line[0]]
-
-    # 출발 노드와 도착 노드가 연결되면 종료
-    while stack[0][0] != start or stack[-1][-1] != end:
-        if 
-
-        # 블록 빼는 부분
-        if stack[-1][-1] 
+    stack = []
+    
+    while stack:
+        if :
+            
+    
 
     # print(f'#{tc+1} {ans}')
