d25eaf85ec8f61209dd0db80d6dc6bdc9781fef0
lim kyoung tae <limkt0925@gmail.com>
2024-02-05 00:32:41 +0900

    algorithm study COMPLETE

--- 02_algorithm/04_List_II/1954_snail/1954.py
+++ 02_algorithm/04_List_II/1954_snail/1954.py
@@ -1,8 +1,6 @@
 # 1954 달팽이 숫자
 
-T = int(input())
-
-for case in range(1, T+1):
+for tc in range(int(input())):
     N = int(input())
     snail = [[0 for _ in range(N)] for _ in range(N)]
     stock = 0
@@ -26,6 +24,44 @@ for case in range(1, T+1):
     if N % 2 == 1:
         snail[N//2][N//2] = N**2
 
-    print(f'#{case}')
+    print(f'#{tc+1}')
     for num in range(N):
         print(*snail[num])
+
+
+'''
+# 델타 탐색
+
+for tc in range(int(input())):
+    #    우  하  좌  상
+    di = [0, 1, 0, -1]
+    dj = [1, 0, -1, 0]
+    nj = 0
+    i, j = 0, 0
+
+    N = int(input())
+    snail = [[0 for _ in range(N)] for _ in range(N)]
+
+    for num in range(1, N+1):
+        # 현재 위치에 숫자 채우기
+        snail[i][j] = num
+
+        # 다음 위치 계산
+        next_row, next_col = i + di[nj], j + dj[nj]
+
+        # 다음 위치가 범위를 벗어나거나 이미 숫자가 채워져 있는 경우 방향 변경
+        if (
+            next_row < 0 or next_row >= N or
+            next_col < 0 or next_col >= N or
+            snail[next_row][next_col]
+        ):
+            nj = (nj+1) % 4
+            next_row, next_col = i + di[nj], j + dj[nj]
+        
+        # 다음 위치로 이동
+        i, j = next_row, next_col
+
+    print(f'#{tc+1}')
+    for num in range(N):
+        print(*snail[num])
+'''
