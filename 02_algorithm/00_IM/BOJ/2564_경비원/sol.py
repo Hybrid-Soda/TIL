ff0fea10611da63212bde5ae2353b22f69f9492b
임경태 <limkt0925@gmail.com>
2024-02-25 01:15:25 +0900

    240225 UPDATE

--- 02_algorithm/00_IM/BOJ/2564_경비원/sol.py
+++
@@ -1,7 +0,0 @@
-import sys
-sys.stdin = open('input.txt')
-
-w, h = map(int, input().split())  # 가로, 세로
-n = int(input())  # 상점 개수
-store = [list(map(int, input().split())) for _ in range(n)]  # 상점 위치 (방향, 인덱스)
-d, i = map(int, input().split())  # 동근 위치
