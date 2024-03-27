d76e0c54934d631911ff943ddc326858630db863
limkt <limkt0925@gmail.com>
2024-03-27 16:37:23 +0900

    240327 UPDATE

---
+++ 05_Django/06_Django_Form/99_online/articles/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Article
+
+class ArticleForm(forms.ModelForm):
+    title = forms.CharField(
+        label='제목',
+        widget=forms.TextInput(
+            attrs={
+                'class': 'my-title',
+                'placeholder': 'Enter the title',
+                'maxlength': 10,
+            }
+        ),
+    )
+    content = forms.CharField(
+        label='내용',
+        widget=forms.Textarea(),
+        error_messages={'required':'내용을 입력해주세요.'},
+    )
