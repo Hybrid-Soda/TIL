de2561bac67e0b33d0ce564c8c75b5894c40adcc
임경태 <limkt0925@gmail.com>
2024-05-06 23:08:33 +0900

    240508 UPDATE

---
+++ 08_Vue/05_Component_State_Flow/vue_hw_5_4/src/components/ChildPage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h3>자식 페이지입니다.</h3>
+    <p>이름 : {{ childrenProp.name }}</p>
+    <p>나이 : {{ childrenProp.age }}</p>
+    <p>용돈 : {{ childrenProp.balance }}</p>
+    <button @click="giveMeAllowance(childrenProp)">용돈 주세요.</button>
+  </div>
+</template>
+
+<script setup>
+  const emit = defineEmits(['giveMeAllowance'])
+  const giveMeAllowance = function (children) {
+    emit('giveMeAllowance', children)
+  }
+
+  defineProps({
+    childrenProp: Object
+  })
+</script>
+
+<style scoped>
+</style>
