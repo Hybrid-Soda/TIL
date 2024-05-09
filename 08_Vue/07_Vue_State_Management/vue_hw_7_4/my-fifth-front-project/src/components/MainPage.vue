46420d7288b5dfdd3d45dd3d21e28152b2157f29
임경태 <limkt0925@gmail.com>
2024-05-09 18:00:26 +0900

    240509 UPDATE

---
+++ 08_Vue/07_Vue_State_Management/vue_hw_7_4/my-fifth-front-project/src/components/MainPage.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <p>이름: {{ balance.name }}</p>
+    <p>잔고: {{ balance.balance }}</p>
+    <RouterLink :to="{ name: 'update', params: {'name': balance.name} }">
+      <button>updateBalance</button>
+    </RouterLink>
+    <hr>
+  </div>
+</template>
+
+<script setup>
+defineProps({
+  balance: Object
+})
+</script>
---
+++ 08_Vue/07_Vue_State_Management/vue_ws_7_2/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <header class="header">
+    <div>
+      <nav class="nav">
+        <RouterLink :to="{ name: 'home' }" class="nav-link">Home</RouterLink>
+        <RouterLink :to="{ name: 'products' }" class="nav-link">Products</RouterLink>
+        <RouterLink :to="{ name: 'favorite' }" class="nav-link">Favorite</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <main class="main">
+    <RouterView />
+  </main>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+</script>
+
+<style scoped>
+.header {
+  background-color: #333;
+  color: white;
+  padding: 10px 0;
+}
+
+.wrapper {
+  max-width: 1200px;
+  margin: 0 auto;
+}
+
+.nav {
+  display: flex;
+  justify-content: center;
+}
+
+.nav-link {
+  color: white;
+  text-decoration: none;
+  margin: 0 15px;
+  font-size: 18px;
+}
+
+.main {
+  padding: 20px;
+}
+</style>
