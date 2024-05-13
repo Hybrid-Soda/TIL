b2cc0b7d93784f2bc359a0c84fe420f02bec0d69
임경태 <limkt0925@gmail.com>
2024-05-13 17:02:09 +0900

    240513 UPDATE

---
+++ 08_Vue/08_Vue_with_DRF_1/vue_hw_8_4_VUE_DRF_연동/front/src/components/ArticleList.vue
@@ -0,0 +1,28 @@
+<template>
+  <div 
+    v-for="article, idx  in store.articles"
+    :key="article.id"
+    >
+    <h3>{{ idx+1 }}번 게시글</h3>
+    <p>제목 : {{ article.title }}</p>
+    <p>내용 : {{ article.content }}</p><hr>
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from 'vue'
+import { useArticleStore } from '@/stores/articles'
+const store = useArticleStore()
+
+onMounted(() => {
+  store.getArticles()
+})
+
+defineProps({
+  article: Object
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
---
+++ 08_Vue/08_Vue_with_DRF_1/vue_hw_8_4_VUE_DRF_연동/front/src/views/ArticleCreateView.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>게시글 생성 페이지</h1>
+    <form @submit.prevent="store.createArticle(title, content)">
+      <label for="title">제목 : </label>
+      <input type="text" id="title" v-model.trim="title">
+      <label for="content">내용 : </label>
+      <input type="text" id="content" v-model.trim="content">
+      <input type="submit">
+    </form>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+import { useArticleStore } from '@/stores/articles'
+
+const title = ref(null)
+const content = ref(null)
+const store = useArticleStore()
+</script>
+
+<style scoped>
+
+</style>
