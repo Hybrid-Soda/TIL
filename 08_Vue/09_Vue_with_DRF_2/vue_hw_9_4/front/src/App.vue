a49e33b4cf6851e2ffe9e34ad7cdab849b4c0137
임경태 <limkt0925@gmail.com>
2024-05-14 17:41:50 +0900

    240514 UPDATE

---
+++ 08_Vue/09_Vue_with_DRF_2/vue_hw_9_4/front/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <header>
+    <div class="wrapper">
+      <nav>
+        <RouterLink :to="{ name: 'home' }">Home</RouterLink> | 
+        <RouterLink :to="{ name: 'signUp' }">SignUp</RouterLink> | 
+        <RouterLink :to="{ name: 'logIn' }">SignIn</RouterLink>
+      </nav>
+    </div>
+  </header>
+
+  <RouterView />
+</template>
+
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<style scoped>
+</style>
